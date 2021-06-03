b48e090c827579477c6181a028c48f744a31c763
ANDRYXA553 <andriysivik5@gmail.com>
2021-06-03 16:34:38 +0300

    hw3 hw3

--- hw/hw3/src/app/app.module.ts
+++ hw/hw3/src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {RouterModule, Routes} from "@angular/router";
+import { UsersComponent } from './modules/users/components/users/users.component';
+
+
+let routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'users', loadChildren:() =>  import('./modules/users/users.module').then(m=>m.UsersModule) },
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
